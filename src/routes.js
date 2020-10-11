bf192f4b1cc960aebfdacf87b6d7bcf79d9eb921
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-11 11:22:22 +0300

    add auth routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ import { Switch, Route } from "react-router-dom";
 import GlobalFeed from "../src/pages/globalFeed";
 import Article from "./pages/article";
 import React from "react";
+import Authentication from "./pages/authentication";
 
 export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
+      <Route path="/login" component={Authentication} />
+      <Route path="/register" component={Authentication} />
       <Route path="/articles/:slug" component={Article} />
     </Switch>
   );
