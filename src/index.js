323a999b350c0ee3dbe63bb7b7aa85c6b46d7ac2
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-11 15:05:45 +0300

    add provider

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom";
 import Routes from "../src/routes";
 import TopBar from "../src/pages/components/topBar";
 import { BrowserRouter as Router } from "react-router-dom";
+import {CurrentUserProvider} from '../src/contexts/currentUser'; 
 
 const App = () => {
   return (
-    <div>
+    <CurrentUserProvider>
       <Router>
         <TopBar/>
         <Routes />
       </Router>
-    </div>
+    </CurrentUserProvider>
   );
 };
-//006
+//020
 ReactDOM.render(
   <React.StrictMode>
     <App />
