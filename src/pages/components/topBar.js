992ed2ed38fb8d4e5bec3e6ada696d3984fb2792
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-11 11:17:35 +0300

    base menu mplemented

--- src/pages/components/topBar.js
+++ src/pages/components/topBar.js
@@ -1,7 +1,25 @@
 import React from 'react'; 
+import {Link, NavLink} from 'react-router-dom'; 
 
 const TopBar = () => {
-    return <div>TopBar here!</div>
+    return (
+    <nav className = 'navbar navbar-light'>
+     <div className='container'>
+        <Link to='/' className='navbar-brand'>Medium Clone</Link>
+        <ul className = 'nav navbar-nav pull-xs-right'>
+            <li className='nav-item'>
+            <NavLink to='/' className='nav-link' exact>Home</NavLink>
+            </li>
+            <li className='nav-item'>
+            <NavLink to='/login' className='nav-link' exact>Login</NavLink>
+            </li>
+            <li className='nav-item'>
+            <NavLink to='/register' className='nav-link' exact>Register</NavLink>
+            </li>
+        </ul>
+     </div> 
+    </nav>
+    ); 
 }
 
 export default TopBar;
