3485a00d05d6c3929e0c358f26b2c0aa09ddf135
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-15 09:18:01 +0300

    make topBar better!

--- src/pages/components/topBar.js
+++ src/pages/components/topBar.js
@@ -1,7 +1,10 @@
-import React from 'react'; 
+import React, { useContext, Fragment } from 'react'; 
 import {Link, NavLink} from 'react-router-dom'; 
+import { CurrentUserContext } from '../../contexts/currentUser';
 
 const TopBar = () => {
+    const [currentUserState] = useContext(CurrentUserContext); 
+
     return (
     <nav className = 'navbar navbar-light'>
      <div className='container'>
@@ -10,12 +13,32 @@ const TopBar = () => {
             <li className='nav-item'>
             <NavLink to='/' className='nav-link' exact>Home</NavLink>
             </li>
-            <li className='nav-item'>
-            <NavLink to='/login' className='nav-link'>Login</NavLink>
-            </li>
-            <li className='nav-item'>
-            <NavLink to='/register' className='nav-link'>Register</NavLink>
-            </li>
+            {currentUserState.isLoggedIn === false? (
+                <Fragment>
+                    <li className='nav-item'>
+                    <NavLink to='/login' className='nav-link'>Login</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                    <NavLink to='/register' className='nav-link'>Register</NavLink>
+                    </li>
+                </Fragment>
+            ): null}
+             {currentUserState.isLoggedIn === true ? (
+                <Fragment>
+                    <li className='nav-item'>
+                    <NavLink to='/articles/new' className='nav-link'>
+                        <i className='ion-compose'></i>
+                        &nbsp; New Post
+                    </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                    <NavLink to={`/profiles/${currentUserState.currentUser.username}`} className='nav-link'>
+                       <img alt ='' className = 'user-pic' src = {currentUserState.currentUser.image} />
+                       &nbsp; {currentUserState.currentUser.username}                       
+                    </NavLink>
+                    </li>
+                </Fragment>
+            ): null}
         </ul>
      </div> 
     </nav>
