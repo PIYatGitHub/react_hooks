c1a1be003463497814d0164424152708bff1f85e
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-15 22:34:58 +0300

    make global feed work

--- src/pages/globalFeed/index.js
+++ src/pages/globalFeed/index.js
@@ -1,7 +1,42 @@
-import React from 'react'; 
-
+import React, { useEffect } from 'react'; 
+import useFetch from '../../hooks/useFetch'; 
+import Feed from '../components/feed';
 const GlobalFeed = () => {
-    return <div>GlobalFeed here!</div>
+    const apiUrl = '/articles?limit=10&offset=0'; 
+    const [{response, isLoading, error}, doFetch] = useFetch(apiUrl);
+    
+    useEffect(()=> {
+        doFetch(); 
+    }, [doFetch])
+    
+    
+    return (
+    <div className = 'home-page'>
+        <div className='banner'>
+            <div className='container'>
+            <h1>Medium clone</h1>
+            <p>Your knowledge share point</p>
+        </div>
+        </div>
+        
+        <div className='container page'>
+            <div className='row'>
+                <div className='col-md-9'>
+                    {isLoading? <div>Page loading...</div>: null}
+                    {error? <div>Unknown error occurred...</div>: null}
+                    {!isLoading && !error && response? 
+                    <Feed articles = {response.articles}/>: 
+                    null
+                    }
+                </div>
+                <div className='col-md-3'>
+                    Popular tags here!
+                </div>
+            </div>
+
+        </div>
+    </div>
+    )
 }
 
 export default GlobalFeed;
