265a35c9dfaeb5cf563caa48bee88b8e6c51db2f
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-15 08:47:29 +0300

    no log

--- src/pages/components/backendErrorMessages.js
+++ src/pages/components/backendErrorMessages.js
@@ -1,9 +1,6 @@
 import React from 'react'; 
 
 const BackendErrorMessages = ({backendErrors}) => {
-    console.log('DATA IN? ', backendErrors);
-    // console.log('DATA IN? ', backendErrors.backendErrors);
-    // let data = backendErrors.backendErrors; 
     const errorMessages = Object.keys(backendErrors).map(name=>{
         const messages = backendErrors[name].join(' ');
         return `${name} ${messages}`;
