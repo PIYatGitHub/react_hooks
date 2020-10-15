30b33177da17707ecae5fa5562e7e5d398f26332
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-15 08:48:16 +0300

    index update

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import Routes from "../src/routes";
 import TopBar from "../src/pages/components/topBar";
 import { BrowserRouter as Router } from "react-router-dom";
 import {CurrentUserProvider} from '../src/contexts/currentUser'; 
+import CurrentUserChecker from "./pages/components/currentUserChecker";
 
 const App = () => {
   return (
     <CurrentUserProvider>
-      <Router>
+      <CurrentUserChecker>
+         <Router>
         <TopBar/>
         <Routes />
       </Router>
+      </CurrentUserChecker>     
     </CurrentUserProvider>
   );
 };
-//020
+//021 / 041
 ReactDOM.render(
   <React.StrictMode>
     <App />
