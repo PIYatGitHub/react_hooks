545e783a01477d4265ec1bec612831312463cfa6
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-18 17:29:05 +0300

    edit route

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,14 @@ import Article from "./pages/article";
 import React from "react";
 import Authentication from "./pages/authentication";
 import CreateArticle from "./pages/createArticle";
+import EditArticle from "./pages/editArticle";
 
 export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
       <Route path="/articles/new" component={CreateArticle} />
+      <Route path="/articles/:slug/edit" component={EditArticle} exact/>
       <Route path="/tags/:slug" component={TagFeed} />
       <Route path="/feed" component={YourFeed} />
       <Route path="/login" component={Authentication} />
