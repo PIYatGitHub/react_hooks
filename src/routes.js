e26eb289a2edeb3dc04da53da1105c26ea11c11c
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2020-10-18 11:12:20 +0300

    new route

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Switch, Route } from "react-router-dom";
 import GlobalFeed from "../src/pages/globalFeed";
+import TagFeed from "../src/pages/tagFeed";
 import Article from "./pages/article";
 import React from "react";
 import Authentication from "./pages/authentication";
@@ -8,6 +9,7 @@ export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
+      <Route path="/tags/:slug" component={TagFeed} />
       <Route path="/login" component={Authentication} />
       <Route path="/register" component={Authentication} />
       <Route path="/articles/:slug" component={Article} />
